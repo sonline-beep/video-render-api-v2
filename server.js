44c5efc40fd6a755063cb6b472f3fe9495464bbe
sonline-beep <melissasoaresonline@gmail.com>
2026-07-23 21:32:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
+const express = require("express");
 
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+    res.json({
+        status: "ok",
+        message: "API funcionando!"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
