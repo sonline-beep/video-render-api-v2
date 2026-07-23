269053e0f59b242d0072f3608a4d6313ddd51fb3
sonline-beep <melissasoaresonline@gmail.com>
2026-07-23 22:14:36 +0200

    Update download.js

--- utils/download.js
+++ utils/download.js
@@ -1,1 +1,29 @@
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
 
+async function downloadFile(url, destino) {
+
+    const writer = fs.createWriteStream(destino);
+
+    const response = await axios({
+        url,
+        method: "GET",
+        responseType: "stream"
+    });
+
+    response.data.pipe(writer);
+
+    return new Promise((resolve, reject) => {
+
+        writer.on("finish", resolve);
+
+        writer.on("error", reject);
+
+    });
+
+}
+
+module.exports = {
+    downloadFile
+};
